c3052595a0a88311a7ef4c8d60cdcf8ca486b7ac
damienbod <damien_bod@hotmail.com>
2022-04-30 11:36:29 +0200

    clean up

--- WebApis/Startup.cs
+++ WebApis/Startup.cs
@@ -47,38 +47,8 @@ public class Startup
 
             options.TokenValidationParameters = new TokenValidationParameters()
             {
-                ClockSkew = new System.TimeSpan(0, 0, 30)
+                ClockSkew = new TimeSpan(0, 0, 30)
             };
-            //options.Events = new JwtBearerEvents()
-            //{
-            //    // TODO return the www-authenticate header
-            //    OnChallenge = context =>
-            //    {
-            //        context.HandleResponse();
-            //        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            //        context.Response.ContentType = "application/json";
-
-            //        // Ensure we always have an error and error description.
-            //        if (string.IsNullOrEmpty(context.Error))
-            //            context.Error = "invalid_token";
-            //        if (string.IsNullOrEmpty(context.ErrorDescription))
-            //            context.ErrorDescription = "This request requires a valid JWT access token to be provided";
-
-            //        // Add some extra context for expired tokens.
-            //        if (context.AuthenticateFailure != null && context.AuthenticateFailure.GetType() == typeof(SecurityTokenExpiredException))
-            //        {
-            //            var authenticationException = context.AuthenticateFailure as SecurityTokenExpiredException;
-            //            context.Response.Headers.Add("x-token-expired", authenticationException?.Expires.ToString("o"));
-            //            context.ErrorDescription = $"The token expired on {authenticationException?.Expires.ToString("o")}";
-            //        }
-
-            //        return context.Response.WriteAsync(JsonSerializer.Serialize(new
-            //        {
-            //            error = context.Error,
-            //            error_description = context.ErrorDescription
-            //        }));
-            //    }
-            //};
         });
     
         services.AddCors(options =>
